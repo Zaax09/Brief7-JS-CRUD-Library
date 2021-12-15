af713f07e84fa2f5387d5b0ecd4eba6e4b2da407
Zakaria <92021986+Zaax09@users.noreply.github.com>
2021-12-15 13:14:37 +0100

    Upqdte Push

--- script.js
+++ script.js
@@ -23,7 +23,6 @@
       const errorElementCover = document.getElementById('errorr7')
       const errorElementDate = document.getElementById('errorr8')
 
-     
 
 
       // 000000000000000000000000000000000000000000000000000000000000000000000000000000000000
@@ -39,6 +38,7 @@
       form.addEventListener('submit', (e) => {
          if(lng.value == ""){
             errorElementLanguage.innerHTML="You must choose the Language for Book"
+            
          }
  
          else{
@@ -91,13 +91,6 @@
 
 
 
-
-
-
-
-
-      
-
       // -------------- Author -----------
 
 
@@ -133,7 +126,7 @@
 
       // -------- Type  ----------
 
-      var types = document.getElementsByClassName("1")
+      var types = document.getElementsByClassName("Types")
 
       form.addEventListener('submit', (e) => {
          if (!(types[0].checked || types[1].checked || types[2].checked)){
@@ -184,79 +177,10 @@
 
 
 
-
-
-
-
-
-
-      //  ------------  Description --------------
-
-
-      form.addEventListener('submit', (e) => {
-
-         let messages = []
-         if (Description.value === '' || Description.value === null) {
-            messages.push('You must write Description of Book  *')
-         }
-
-         if(messages.length > 0){
-            e.preventDefault()
-            errorElementDescription.innerText  = messages.join(', ')
-         }
-         
-      else {
-         errorElementDescription.innerHTML =("Good");
-         errorElementDescription.style.color ="green";
-      }
-
-      })
-
-
-
-
-
-
-
-
-
-
-   // =======================   cover   ==================
-
-
-
-   form.addEventListener('submit', (e) => {
-
-      let messages = []
-      if (Cover.value === '' || Cover.value === null) {
-         messages.push('You must choose Cover For Book  *')
-      }
-
-      if(messages.length > 0){
-         e.preventDefault()
-         errorElementCover.innerText  = messages.join(', ')
-      }
-      else {
-         errorElementCover.innerHTML =("Good");
-         errorElementCover.style.color ="green";
-      }
-
-   })
-
-
-
-
-
-
-
-
-
-
-
-
    
 
    // =======================   date   ==================
+
 
 
 
@@ -282,3 +206,118 @@
 
    })
 
+
+
+
+
+   
+   // =======================   ADD   ==================
+  
+
+   var selectedRow = null
+
+function onFormSubmit()
+{
+    var formData = readFormData();
+    if(selectedRow == null)
+    {
+        insertNewRecord(formData);
+    }
+    else
+    {
+        updateRecord(formData);
+    }
+    resetForm();
+}
+function readFormData()
+{
+    var formData = {};
+    formData['language'] = document.getElementById('language').value;
+    formData['BookTitle'] = document.getElementById('BookTitle').value;
+    formData['Author'] = document.getElementById('Author').value;
+    
+    formData['Types'] = document.getElementsByClassName('Types').value;
+    formData['price'] = document.getElementById('price').value;
+    formData['date'] = document.getElementById('date').value;
+
+    return formData;
+}
+
+
+
+
+
+// fffffffffffffffffffffffffffffffffffffffffffffffffff
+
+
+function insertNewRecord(data)
+{
+    var table = document.getElementById('fruit-list').getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.language;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.BookTitle;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.Author;
+
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.Types;
+    cell5 = newRow.insertCell(4);
+    cell5.innerHTML = data.price;
+    cell6 = newRow.insertCell(5);
+    cell6.innerHTML = data.date;
+    cell7 = newRow.insertCell(6);
+    cell7.innerHTML = `<button id = 'Editt'  onClick='onEdit(this)'>Edit</button> <button onClick='onDelete(this)'>Delete</button>`
+   }
+
+
+
+// llllllllllllllllllllllllllllllllllllllll
+
+
+
+
+
+function resetForm()
+{
+    document.getElementById('language').value = '';
+    document.getElementById('BookTitle').value = '';
+    document.getElementById('Author').value = '';
+
+    document.getElementsByClassName('Types').value = '';
+    document.getElementById('price').value = '';
+    document.getElementById('date').value = '';
+
+    selectedRow = null;
+}
+function onEdit(td)
+{
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById('language').value = selectedRow.cells[0].innerHTML;
+    document.getElementById('BookTitle').value = selectedRow.cells[1].innerHTML;
+    document.getElementById('Author').value = selectedRow.cells[2].innerHTML;
+
+    
+    document.getElementsByClassName('Types').value = selectedRow.cells[3].innerHTML;
+    document.getElementById('price').value = selectedRow.cells[4].innerHTML;
+    document.getElementById('date').value = selectedRow.cells[5].innerHTML;
+}
+function updateRecord(formData)
+{
+    selectedRow.cells[0].innerHTML = formData.language;
+    selectedRow.cells[1].innerHTML = formData.BookTitle;
+    selectedRow.cells[2].innerHTML = formData.Author;
+
+    selectedRow.cells[3].innerHTML = formData.Types;
+    selectedRow.cells[4].innerHTML = formData.price;
+    selectedRow.cells[5].innerHTML = formData.date;
+}
+function onDelete(td)
+{
+    row = td.parentElement.parentElement;
+    document.getElementById('fruit-list').deleteRow(row.rowIndex);
+    resetForm();
+    if(confirm('Are You Sure')){}
+
+}
